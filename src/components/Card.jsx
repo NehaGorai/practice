28b7e2309a33b241fa98ca1f015137858032551b
Neha Gorai <neha@lowcodeapi.com>
2025-07-24 19:21:13 +0530

    remove one consumer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import Details from './Details'
-import { Mydata2 } from '../App'
+
 function Card() {
   return (
 <>
 <Details/>
-<Mydata2.Consumer>
- {(val)=>{
-return(
-    <>
-    <h1>Email: {val.email}</h1></>
-)
- }}
-</Mydata2.Consumer>
+
 
 </>
   )
